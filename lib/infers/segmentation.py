d3dcba3952a3ed63742e013435ce0458894e88b6
Sofia Sappia <sofisappia@gmail.com>
2023-02-07 16:29:02 -0300

    Add original files in repo

---
+++ lib/infers/segmentation.py
@@ -0,0 +1,80 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Callable, Sequence
+
+from monai.inferers import Inferer, SlidingWindowInferer
+from monai.transforms import (
+    Activationsd,
+    AsDiscreted,
+    EnsureChannelFirstd,
+    EnsureTyped,
+    KeepLargestConnectedComponentd,
+    LoadImaged,
+    ScaleIntensityRanged,
+    Spacingd,
+)
+
+from monailabel.interfaces.tasks.infer_v2 import InferType
+from monailabel.tasks.infer.basic_infer import BasicInferTask
+from monailabel.transform.post import Restored
+
+
+class Segmentation(BasicInferTask):
+    """
+    This provides Inference Engine for pre-trained segmentation (UNet) model over MSD Dataset.
+    """
+
+    def __init__(
+        self,
+        path,
+        network=None,
+        target_spacing=(1.0, 1.0, 1.0),
+        type=InferType.SEGMENTATION,
+        labels=None,
+        dimension=3,
+        description="A pre-trained model for volumetric (3D) segmentation over 3D Images",
+        **kwargs,
+    ):
+        super().__init__(
+            path=path,
+            network=network,
+            type=type,
+            labels=labels,
+            dimension=dimension,
+            description=description,
+            **kwargs,
+        )
+        self.target_spacing = target_spacing
+
+    def pre_transforms(self, data=None) -> Sequence[Callable]:
+        return [
+            LoadImaged(keys="image", reader="ITKReader"),
+            EnsureTyped(keys="image", device=data.get("device") if data else None),
+            EnsureChannelFirstd(keys="image"),
+            Spacingd(keys="image", pixdim=self.target_spacing),
+            ScaleIntensityRanged(keys="image", a_min=-175, a_max=250, b_min=0.0, b_max=1.0, clip=True),
+        ]
+
+    def inferer(self, data=None) -> Inferer:
+        return SlidingWindowInferer(roi_size=self.roi_size)
+
+    def post_transforms(self, data=None) -> Sequence[Callable]:
+        largest_cc = False if not data else data.get("largest_cc", False)
+        t = [
+            EnsureTyped(keys="pred", device=data.get("device") if data else None),
+            Activationsd(keys="pred", softmax=True),
+            AsDiscreted(keys="pred", argmax=True),
+        ]
+        if largest_cc:
+            t.append(KeepLargestConnectedComponentd(keys="pred"))
+        t.append(Restored(keys="pred", ref_image="image"))
+        return t
